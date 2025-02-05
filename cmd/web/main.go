73ed9a9849a0406833d7b8dd62f49fe9186a9959
Nikita <obrikash@gmail.com>
2025-02-05 12:55:09 +0300

    basic handlers, file server

---
+++ cmd/web/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "log"
+    "net/http"
+)
+
+func main() {
+    mux := http.NewServeMux()
+    mux.HandleFunc("/", home)
+    mux.HandleFunc("/snippet/view", snippetView)
+    mux.HandleFunc("/snippet/create", snippetCreate)
+    
+    fileServer := http.FileServer(http.Dir("./ui/static/"))
+    mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+    log.Print("Starting server on localhost:4000")
+    err := http.ListenAndServe("localhost:4000", mux)
+    log.Fatal(err)
+}
